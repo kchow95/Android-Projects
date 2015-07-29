60389f0a05515e38b863c12e04fe2d3d662a1bf4
Kenneth Chow <kjc2577@utexas.edu>
2015-07-29 18:43:47 -0500

    initial commit

---
+++ Nocrastinator/app/src/main/java/chow/nocrastinator/MainActivity.java
@@ -0,0 +1,117 @@
+package chow.nocrastinator;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private List<Task> tasks= new ArrayList<Task>();
+    private static final String TAG = "Test";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 18);
+        calendar.set(Calendar.MINUTE, 45);
+        calendar.set(Calendar.SECOND, 0);
+        Intent intent1 = new Intent(MainActivity.this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0,intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager am = (AlarmManager) MainActivity.this.getSystemService(Context.ALARM_SERVICE);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        setContentView(R.layout.activity_main);
+
+        Task task = new Task();
+        task.setName("Test");
+
+        ArrayAdapter<Task> adapter = new TaskListAdapter(this, R.layout.alarm_single, tasks);
+        adapter.notifyDataSetChanged();
+
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+        tasks.add(task);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class TaskListAdapter extends ArrayAdapter<Task> {
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            final Task task = tasks.get(position);
+            View itemView = convertView;
+
+            //make sure to have a view to work with
+            if (itemView == null){
+                itemView = getLayoutInflater().inflate(R.layout.alarm_single, parent, false);
+            }
+
+            TextView nameText = (TextView) itemView.findViewById(R.id.task_name);
+            nameText.setText(task.getName());
+
+            Switch taskSwitch = (Switch) itemView.findViewById(R.id.task_switch);
+            taskSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    Log.i(TAG, "checked");
+                }
+            });
+
+
+
+
+            return itemView;
+        }
+
+        public TaskListAdapter(Context context,int resource, List<Task> objects)
+        {
+            super(context, resource, objects);
+        }
+        @Override
+        public int getCount()
+        {
+            return tasks.size();
+        }
+    }
+}
---
+++ Nocrastinator/app/src/main/java/chow/nocrastinator/Task.java
@@ -0,0 +1,52 @@
+package chow.nocrastinator;
+
+import java.util.Calendar;
+
+/**
+ * Created by kchow95 on 7/29/15.
+ */
+public class Task {
+    private String name, message;
+    private Calendar startTime, endTime;
+    private int duration;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Calendar getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Calendar startTime) {
+        this.startTime = startTime;
+    }
+
+    public Calendar getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Calendar endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
